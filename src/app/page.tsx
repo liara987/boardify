fbcf4c56a615d9e1133a9b293c097adcfef02a2b
liara987 <987.liara@gmail.com>
2024-12-18 20:39:01 -0300

    feature: add variation to button components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,26 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant={"outline"} size="sm">
-        Click here
+      <Button variant={"primary"} size="lg">
+        primary
+      </Button>
+      <Button variant={"secondary"} size="lg">
+        secondary
+      </Button>
+      <Button variant={"destructive"} size="lg">
+        destructive
+      </Button>
+      <Button variant={"ghost"} size="lg">
+        ghost
+      </Button>
+      <Button variant={"muted"} size="lg">
+        muted
+      </Button>
+      <Button variant={"outline"} size="lg">
+        outline
+      </Button>
+      <Button variant={"teritary"} size="lg">
+        teritary
       </Button>
     </div>
   );
