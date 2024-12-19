eb2177bb9f9cfd939efbed7853c0825327f4d0d6
liara987 <987.liara@gmail.com>
2024-12-19 12:02:18 -0300

    feature: create sign-in layout

---
+++ src/app/(auth)/sign-in/layout.tsx
@@ -0,0 +1,8 @@
+interface SignInLayoutProps {
+  children: React.ReactNode;
+}
+const SignInLayout = ({ children }: SignInLayoutProps) => {
+  return <div>{children}</div>;
+};
+
+export default SignInLayout;
