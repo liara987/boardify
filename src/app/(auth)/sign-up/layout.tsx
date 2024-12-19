233fcd7c6ecc8c3880e75d96e5ef87fa8f0e5af4
liara987 <987.liara@gmail.com>
2024-12-19 12:01:53 -0300

    feature: create sign-up layout

---
+++ src/app/(auth)/sign-up/layout.tsx
@@ -0,0 +1,8 @@
+interface SignUpLayoutProps {
+  children: React.ReactNode;
+}
+const SignUpLayout = ({ children }: SignUpLayoutProps) => {
+  return <div>{children}</div>;
+};
+
+export default SignUpLayout;
