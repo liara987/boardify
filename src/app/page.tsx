15af81e71dd0d455afcef6373efc5906586de1ef
liara987 <987.liara@gmail.com>
2025-01-09 21:20:30 -0300

    feat: create user button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import { useCurrent } from "@/features/auth/api/use-current";
 import { useLogout } from "@/features/auth/api/use-logout";
+import { UserButton } from "@/features/auth/components/user-button";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -16,8 +16,7 @@ export default function Home() {
 
   return (
     <div>
-      Only visible to loged users
-      <Button onClick={() => mutate()}>Logout</Button>
+      <UserButton />
     </div>
   );
 }
